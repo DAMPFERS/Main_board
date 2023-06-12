047455757727ea1921fc8fb919c94b015dc16e29
DAMPFERS <razuvaevvladislav060@gmail.com>
2023-06-12 21:13:26 +0700

    Nextion + KArno + TrueTable

--- read_pin/read_pin.ino
+++ read_pin/read_pin.ino
@@ -5,23 +5,28 @@
 #define IN3 4
 #define STEP 8
 #define COUNTER 16
+#define NAMBER_TABLE 3
 #define RX 10
 #define TX 11
 
 void sendingScreen();
 void gameTact();
 int connectCheck(int pin, bool adc);
+bool findTrueTable(byte seg, byte table);
+void comandEnd();
+void SendInt(String dev, int dat);
 
 SoftwareSerial mySerial(RX,TX);
 
-byte karno_table[3][4] = {{1,2,3,4}, {11,12,0,1}, {10,9,8,2}};
-int game_table[3][COUNTER];
+byte karno_table[NAMBER_TABLE][4] = {{1,2,3,4}, {11,12,0,1}, {10,9,8,2}};
+int game_table[NAMBER_TABLE][COUNTER];
+int values[NAMBER_TABLE + 1] = {0,0,0,0};
+const char input[] = "123456"; 
 
 
 void setup(){
-  pinMode(IN1, INPUT);
-  pinMode(IN2, INPUT);
-  pinMode(IN3, INPUT);
+  for (int i = 0; i < NAMBER_TABLE; i++)
+    pinMode(input[i], INPUT);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
@@ -50,18 +55,81 @@ int connectCheck(int pin, bool adc){
     return digitalRead(pin);
 }
 void gameTact(){
-  for(int i = 0; i < COUNTER; i++){
-    game_table[0][i] = connectCheck(IN1,0);
-    game_table[1][i] = connectCheck(IN2,0);
-    game_table[2][i] = connectCheck(IN3,0);
+  for(byte i = 0; i < COUNTER; i++){
+    for(byte j = 0; j < NAMBER_TABLE; j++){
+      game_table[0][i] = connectCheck(input[j],0);
+      if (game_table[0][i] == findTrueTable(i,0))
+        ++values[i];
+    }
     digitalWrite(STEP,HIGH);
     delay(50);
     digitalWrite(STEP,LOW);
   }
-  for(int i = 0; i < 3; i++){
+  for(int i = 0; i < NAMBER_TABLE; i++){
+    values[NAMBER_TABLE + 1] += values[i];
     Serial.println("-----------------------------------------");
     for(int j = 0; j < COUNTER; j++){
       Serial.print(game_table[i][j]);
+      Serial.print(" ");
     }
   }
+  return;
+}
+bool findTrueTable(byte seg, byte table){
+  byte even = 0;
+  byte uneven = 0;
+  byte line;
+  byte column;
+  for(int i = 3; i >= 0; i--){
+    byte bite = bitRead(seg,i);
+    if (i % 2){
+      uneven += bite;
+      uneven = uneven << 1;
+    }
+    else{
+      even += bite;
+      even = even << 1;
+    }
+    switch(even){
+      case 0b00000000:
+        line = 0;
+        break;
+      case 0b00000010:
+        line = 1;
+        break;
+      case 0b00000011:
+        line = 2;
+        break;
+      case 0b00000001:
+        line = 3;
+        break;
+    }
+    switch(uneven){
+      case 0b00000000:
+        column = 0;
+        break;
+      case 0b00000010:
+        column = 1;
+        break;
+      case 0b00000011:
+        column = 2;
+        break;
+      case 0b00000001:
+        column = 3;
+        break;
+    }
+    return bitRead(karno_table[table][line], column);
+  }
+}
+void comandEnd(){ 
+  for(int k = 0; k < 3; k++)
+    mySerial.write(0xff);
+  return; 
+}
+void SendInt(String dev, int dat){
+   Serial.print(dev);
+   Serial.print("=");
+   Serial.print(dat);
+   comandEnd();
+   return;
 }
